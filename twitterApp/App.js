f3f12906708c921269bd41b7af79c308b2a251a0
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-03-13 12:08:18 +0300

    Initial

---
+++ twitterApp/App.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import WelcomeScreen from "./src/screens/WelcomeScreen"
+
+
+
+const App = () => {
+  return (
+      <WelcomeScreen></WelcomeScreen>
+  );
+};
+
+
+
+export default App;
